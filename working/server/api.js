1f4237e684cfb89ad0d7a70a07f4f47bc31cf4a9
Alek Novkovski <alek@greatcode2.com>
2023-05-13 23:34:36 +0200

    Enable api router

--- working/server/api.js
+++ working/server/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-
+apiRouter.get('/', (req, res) => {
+   res.status(200).json({ success: true, data: "it worked!" })
+})
 
 module.exports = apiRouter;
