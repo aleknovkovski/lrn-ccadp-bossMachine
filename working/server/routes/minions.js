4e40eca821269b48dadb40aa106d571191a9efa8
Alek Novkovski <alek@greatcode2.com>
2023-05-14 00:33:10 +0200

    Implement get minion by id

--- working/server/routes/minions.js
+++ working/server/routes/minions.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const minionsRouter = express.Router();
-const {getAllFromDatabase} = require('../db')
+const {getAllFromDatabase, getFromDatabaseById} = require('../db')
 
 minionsRouter.get('/', (req, res) => {
    const minionsData = getAllFromDatabase('minions')
    res.status(200).json({ success: true, data: minionsData })
+})
+
+minionsRouter.get('/:minionId', (req, res) => {
+   const minionId = String(req.params.minionId)
+   const minionData = getFromDatabaseById('minions', minionId)
+   res.status(200).json({ success: true, data: minionData })
 })
 
 module.exports = minionsRouter;
