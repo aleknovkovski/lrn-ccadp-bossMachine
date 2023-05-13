73889b1dd5a60a3faeae0d38832b4b8da925dd0c
Alek Novkovski <alek@greatcode2.com>
2023-05-14 00:42:55 +0200

    Change response format for routes

--- working/server/routes/minions.js
+++ working/server/routes/minions.js
@@ -4,13 +4,13 @@ const {getAllFromDatabase, getFromDatabaseById, addToDatabase} = require('../db'
 
 minionsRouter.get('/', (req, res) => {
    const minionsData = getAllFromDatabase('minions')
-   res.status(200).json({ success: true, data: minionsData })
+   res.status(200).send(minionsData)
 })
 
 minionsRouter.get('/:minionId', (req, res) => {
    const minionId = String(req.params.minionId)
    const minionData = getFromDatabaseById('minions', minionId)
-   res.status(200).json({ success: true, data: minionData })
+   res.status(200).send(minionData)
 })
 
 minionsRouter.post('/', (req, res) => {
