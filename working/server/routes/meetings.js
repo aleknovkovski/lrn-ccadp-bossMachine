4f69620786e8055bb9c1461e0bd02a36714761c4
Alek Novkovski <alek@greatcode2.com>
2023-05-14 13:19:20 +0200

    Add all meetings routes

--- working/server/routes/meetings.js
+++ working/server/routes/meetings.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const meetingsRouter = express.Router();
 
+const {
+   getAllFromDatabase,
+   addToDatabase,
+   deleteFromDatabasebyId,
+   createMeeting
+} = require("../db");
+
 meetingsRouter.get('/', (req, res) => {
-   res.status(200).json({ success: true, data: "it worked for meetings!" })
+   const meetingsData = getAllFromDatabase('meetings')
+   res.status(200).send(meetingsData)
 })
+
+meetingsRouter.post('/', (req, res) => {
+  const newMeeting = addToDatabase('meetings', createMeeting());
+  res.status(201).send(newMeeting);
+})
+meetingsRouter.delete('/:meetingId', (req, res, next) => {
+  const meetingID = String(req.params.meetingId)
+  let deletedSuccessfully = deleteFromDatabasebyId('meetings', meetingID);
+  res.status(201).send(deletedSuccessfully);
+});
 
 module.exports = meetingsRouter;
