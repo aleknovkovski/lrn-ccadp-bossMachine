45d571d8f85cb9277d947dc6d907d0550f615ab3
Alek Novkovski <alek@greatcode2.com>
2023-05-14 13:34:11 +0200

    Write checkMillionDollarIdea

--- working/server/checkMillionDollarIdea.js
+++ working/server/checkMillionDollarIdea.js
@@ -1,4 +1,14 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const numWeeks = req.body.numWeeks
+    const weeklyRevenue = req.body.weeklyRevenue
+    const totalMoney = Number(numWeeks) * Number(weeklyRevenue);
+
+    if (numWeeks && weeklyRevenue && totalMoney >= 1000000) {
+        next();
+    } else {
+        res.status(400).send();
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
